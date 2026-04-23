0ee4471487fe1b6098d4029c558c47c4f42b3785
davmeng <dawitmen@gmail.com>
2026-04-23 17:53:33 +0200

    Added 3 lines to branch

--- firstfile.py
+++ firstfile.py
@@ -12,3 +12,10 @@ print("WARNING! \nBECAREFUL not to save your credentials in github!")
 
 print("OpenAI Key = ", OPENAI_API_KEY)
 print("Activeloop = ", ACTIVELOOP_TOKEN)
+
+my_activeloop_org_id = "dawitm" 
+my_activeloop_dataset_name = "ai_assistant_test3z"
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import DeepLake
+
