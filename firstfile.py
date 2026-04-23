cb4693400fd904b2eb7fca4903909e1997a0caa5
davmeng <dawitmen@gmail.com>
2026-04-23 13:51:54 +0200

    important reminder

--- firstfile.py
+++ firstfile.py
@@ -8,6 +8,7 @@ os.environ["ACTIVELOOP_TOKEN"] = expt2
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 ACTIVELOOP_TOKEN = os.getenv("ACTIVELOOP_TOKEN")
+print("WARNING! \nBECAREFUL not to dsave your credential in github!")
 
 print("OpenAI Key = ", OPENAI_API_KEY)
 print("Activeloop = ", ACTIVELOOP_TOKEN)
